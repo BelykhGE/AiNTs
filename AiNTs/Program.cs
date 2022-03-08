4192790355afecb8e877c18b2603b7d2ea3f3a5c
German Belykh <german7069@ya.ru>
2022-03-08 14:19:47 +0300

    Change default formatting.

--- AiNTs/Program.cs
+++ AiNTs/Program.cs
@@ -7,20 +7,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AiNTs
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+namespace AiNTs {
+    public class Program {
+        public static void Main( string[] args ) {
+            CreateHostBuilder( args ).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+        public static IHostBuilder CreateHostBuilder( string[] args ) =>
+            Host.CreateDefaultBuilder( args )
+                .ConfigureWebHostDefaults( webBuilder => {
                     webBuilder.UseStartup<Startup>();
-                });
+                } );
     }
 }
--- AiNTs/Startup.cs
+++ AiNTs/Startup.cs
@@ -8,33 +8,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AiNTs
-{
-    public class Startup
-    {
+namespace AiNTs {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices( IServiceCollection services ) {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure( IApplicationBuilder app , IWebHostEnvironment env ) {
+            if ( env.IsDevelopment() ) {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseEndpoints( endpoints => {
+                endpoints.MapGet( "/" , async context => {
+                    await context.Response.WriteAsync( "Hello World!" );
+                } );
+            } );
         }
     }
 }
